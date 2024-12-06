bfb622a8ca172dccc7437cae5e6f87ffcc878011
JayanshAdithya <jayansh.adithya@gmail.com>
2024-12-06 14:50:39 +0530

    Updated Code

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
   },
   input: {
     width: "100%",
-    height: 50,
+    height: 45,
     backgroundColor: "#fff",
     borderRadius: 10,
     paddingHorizontal: 15,
